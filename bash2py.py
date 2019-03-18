b34f7eabae72e3b01fcdf36ab37ba38dce3ee106
Michael Wood <mawood20@gmail.com>
2019-03-18 13:04:47 -0400

    Activate help message for no-argument execution

--- bash2py.py
+++ bash2py.py
@@ -2,10 +2,10 @@ import os, sys
 import pdb
 
 def translate(bashFileName, html):
-	os.system('bash-4.3.30/bash2pyengine ' + html + bashFileName)
+	os.system('/usr/share/bash2py/bash2py-3.6/bash-4.3.30/bash2pyengine ' + html + bashFileName)
 
 def helpx():
-	print("Usage:\n./bash2py [-h] [-d] <dir_name>\n./bash2py [-h] [-f] <file_name>\n")
+    print("Usage:\n./bash2py [-h] [-d] <dir_name>\n./bash2py [-h] [-f] <file_name>\n  -h: generate html diff not python code")
 
 def main():
 
@@ -13,6 +13,11 @@ def main():
 	option = ''
 
 	lth = len(sys.argv);
+
+        if (lth == 1):
+		helpx()
+		exit(0)
+
 	bashFileName = sys.argv[lth-1];
 	for i in range(1,lth - 1) :
 		if (sys.argv[i] == '-f' or sys.argv[i] == '-d') :
@@ -23,13 +28,13 @@ def main():
 	if (option == ''):
 		if (os.path.isdir(bashFileName)) :
 			option = '-d'
-		else :
+		elif (os.path.isfile(bashFileName)) :
 			option = '-f'
 
 	if (option == '-f'):
 		translate(bashFileName, html)
 		exit(0)
-		
+
 	if(option == '-d'):
 		for dirname, subDirs, files in os.walk(bashFileName):
 			for f in files:
@@ -37,8 +42,6 @@ def main():
 					bashFileName1 = os.path.join(dirname, f)
 					translate(bashFileName1, html)
 		exit(0)
-
-	helpx()
 
 if __name__ == "__main__":
 	main()
